e6639b98b4d059898e6333b0720e0d5d24b38667
KudzanaiGomera <kgomera@student.wethinkcode.co.za>
2022-04-21 19:11:21 +0200

    done getters and setters in vehicle main class

---
+++ README.md
@@ -0,0 +1,1 @@
+learning OOP using PHP
--- vehicles.php
+++ vehicles.php
@@ -5,33 +5,29 @@
  * 
  */
 
-
-
-class vehicles{
+class Vehicle{
     
-    //properties
-    public $colour = 'white';
-    private $body_type = "";
-    private $engine_size = "";
-    private $wheels ="";
+    // Properties
+    public $colour;
+    private $body_type;
+    private $engine_size;
+    private $wheels;
+    private $regstration_number;
+    private $fuel_type;
     
-    public $dashboard="none";
-    
-   
-    public function __construct()
+   // constructor
+    public function __construct($colour, $body_type, $engine_size, $wheels, $regstration_number, $fuel_type)
     {
-        echo "I'm a vehicle". PHP_EOL;
+        // echo "constructor working..". PHP_EOL;
+        $this->colour = $colour;
+        $this->body_type = $body_type;
+        $this->engine_size = $engine_size;
+        $this->wheels = $wheels;
+        $this->regstration_number = $regstration_number;
+        $this->fuel_type = $fuel_type;
     }
 
-
-    //functions
-    function movement(){
-        $this->colour = "white";
-    }
-
-    function speed(){
-        
-    }
+    // setters and getters
 
     /**
      * @return mixed
@@ -42,15 +38,97 @@ class vehicles{
     }
 
     /**
+     * @return mixed
+     */
+    public function getBodyType()
+    {
+        return $this->body_type;
+    }
+
+     /**
+     * @return mixed
+     */
+    public function getEngineSize()
+    {
+        return $this->engine_size;
+    }
+
+     /**
+     * @return mixed
+     */
+    public function getWheels()
+    {
+        return $this->body_type;
+    }
+
+     /**
+     * @return mixed
+     */
+    public function getRegistrationNumber()
+    {
+        return $this->regstration_number;
+    }
+
+     /**
+     * @return mixed
+     */
+    public function getFuelType()
+    {
+        return $this->fuel_type;
+    }
+
+    /**
      * @param mixed $colour
      */
-    public function setColour($colour): void
+    public function setColour($colour)
     {
         $this->colour = $colour;
     }
+
+    /**
+     * @param mixed $body_type
+     */
+    public function setBodyType($body_type)
+    {
+        $this->body_type = $body_type;
+    }
+
+    /**
+     * @param mixed $engine_size
+     */
+    public function setEngineSize($engine_size)
+    {
+        $this->engine_size = $engine_size;
+    }
+
+    /**
+     * @param mixed $wheels
+     */
+    public function setWheels($wheels)
+    {
+        $this->wheels = $wheels;
+    }
+
+    /**
+     * @param mixed $registration_number
+     */
+    public function setRegistrationNumber($regstration_number)
+    {
+        $this->regstration_number = $regstration_number;
+    }
+
+    /**
+     * @param mixed $fuel_type
+     */
+    public function setFuelType($fuel_type)
+    {
+        $this->fuel_type = $fuel_type;
+    }
+
 }
 
-class bmw extends vehicles{
+//BMW Class
+class bmw extends Vehicle{
     public function __construct()
     {
         echo "I'm a BMW! ". PHP_EOL;
@@ -64,16 +142,12 @@ class bmw extends vehicles{
         return $this->colour." becuase i am nuice " . PHP_EOL;
     }
 }
-$vehicle = new vehicles();
-$bmw = new bmw();
 
-echo "my colour is  ".$vehicle->getColour(). PHP_EOL;
-$vehicle->setColour('pink');
-echo "my colour is  ".$vehicle->getColour(). PHP_EOL;
+// Instantiating a class 
+$vehicle1 = new Vehicle("white", "SUV", "1.4", "4", "GP 23482", "petrol");
+$vehicle1->setColour('black');
+$color = $vehicle1->getColour();
+echo "my vehicle1 color is ".$vehicle1->colour. PHP_EOL;
 
-
-echo "my colour is  ".$bmw->getColour(). PHP_EOL;
-$bmw->setColour('red');
-echo "my colour is  ".$bmw->getColour(). PHP_EOL;
-
+// $bmw = new bmw();
 
